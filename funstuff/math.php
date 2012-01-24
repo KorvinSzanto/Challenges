aaaed24a7a8d007a2116e68a08994b60b898c066
Korvin Szanto <Korvinszanto@gmail.com>
2012-01-24 08:44:21 -0800

    Finished math.php

--- funstuff/math.php
+++ funstuff/math.php
@@ -1,10 +1,54 @@
+<pre>
 <?php
+function divby0($in) { return 'ERROR'; }
+function divby1($in) { return 1; }
+function divby2($in) { return (pow(-1,2) == 1?1:0); }
 function divby3($in) {
 	$num = 0;
 	foreach(preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY) as $n) $num+=$n;
 	if (strlen($num) > 1) return divby3($num);
 	else return (in_array($num,array(3,6,9))?1:0);
 }
+function divby4($in) { return (substr($in,-2)%4==0?1:0); }
+function divby5($in) { return (in_array(substr($in,-1),array(0,5))?1:0); }
+function divby6($in) { return (divby2($in)?(divby3($in)?1:0):0); }
+function divby7($in) {
+	$n = preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY);
+	$f = array_pop($n);
+	$num = 3 * implode('',$n) + $f;
+	if (strlen($num) > 1) return divby3($num);
+	else return ($num == 7)?1:0;
+}
+function divby8($in) { return (substr($in,-3)%8==0?1:0); }
+function divby9($in) {
+	$num = 0;
+	foreach(preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY) as $n) $num+=$n;
+	if (strlen($num) > 1) return divby3($num);
+	else return ($num == 9)?1:0;
+}
+function divby10($in) { return (substr($in,-1) == 0?1:0; }
 
 
-echo divby3(501);
+$nu = 70000;
+echo "$nu\n_____\n0: ";
+echo divby0($nu);
+echo "\n1: ";
+echo divby1($nu);
+echo "\n2: ";
+echo divby2($nu);
+echo "\n3: ";
+echo divby3($nu);
+echo "\n4: ";
+echo divby4($nu);
+echo "\n5: ";
+echo divby5($nu);
+echo "\n6: ";
+echo divby6($nu);
+echo "\n7: ";
+echo divby7($nu);
+echo "\n8: ";
+echo divby8($nu);
+echo "\n9: ";
+echo divby9($nu);
+echo "\n10: ";
+echo divby10($nu);
