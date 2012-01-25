8b40a1c2b1eed86dcd20772224c3c548b2c64f68
Korvin Szanto <Korvinszanto@gmail.com>
2012-01-25 07:57:57 -0800

    Update funstuff/math.php

--- funstuff/math.php
+++ funstuff/math.php
@@ -1,27 +1,32 @@
-<?php
+<pre><?php
 function divby0($in) { return 'ERROR'; }
 function divby1($in) { return 1; }
 function divby2($in) { return (pow(-1,$in) == 1?1:0); }
 function divby3($in) {
-	$num = 0;
-	foreach(preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY) as $n) $num+=$n;
-	if (strlen($num) > 1) return divby3($num);
-	else return (in_array($num,array(3,6,9))?1:0);
+	do {
+		$num = 0;
+		for ($i=0;$i<strlen($in);$i++) $num = $num + substr($in,$i,1);
+		$in = $num;
+	} while(strlen($num) > 1);
+	return (in_array($num,array(3,6,9))?1:0);
 }
 function divby4($in) { return (substr($in,-2)%4==0?1:0); }
 function divby5($in) { return (in_array(substr($in,-1),array(0,5))?1:0); }
 function divby6($in) { return (divby2($in)?(divby3($in)?1:0):0); }
 function divby7($in) {
-	$num = (3 * substr($in,0,strlen($in)-1)) + substr($in,-1);
-	if (strlen($num) > 1) return divby7($num);
-	else return ($num == 7)?1:0;
+	do {
+		$num = (3 * substr($in,0,strlen($in)-1)) + substr($in,-1);
+		$in = $num;
+	} while (strlen($num) > 1);
+	return ($num == 7)?1:0;
 }
 function divby8($in) { return (substr($in,-3)%8==0?1:0); }
 function divby9($in) {
-	$num = 0;
-	foreach(preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY) as $n) $num+=$n;
-	if (strlen($num) > 1) return divby3($num);
-	else return ($num == 9)?1:0;
+	do {
+		$num = 0;
+		foreach(preg_split('//', $in, -1, PREG_SPLIT_NO_EMPTY) as $n) $num+=$n;
+	} while (strlen($num) > 1);
+	return ($num == 9)?1:0;
 }
 function divby10($in) { return (substr($in,-1) == 0?1:0); }
 
